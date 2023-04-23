5fa8c8bdb58792c92416a3504b31ee44f774a7c9
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-04-23 17:10:35 +0900

    update!

--- src/main/java/org/example/Xc.java
+++ src/main/java/org/example/Xc.java
@@ -14,13 +14,25 @@ public class Xc {
         view();
     }
     private static void view() throws IOException {
-        File file = new File("./src/main/html/index.html");
+        File file = new File("./src/main/html/Densan.html");
         Document doc = Jsoup.parse(file);
         String title = doc.title();
         System.out.println("TITLE IS : \"" + title + "\"");
 
         Element body = doc.body();
-        Elements elements = body.getElementsByTag("p");
+        Elements elements = body.getElementsByTag("h2");
+
+        //get Elements By id and class
+        Element elements2 = doc.getElementById("hello");
+        System.out.println("ELEMENT BY ID -> \"hello\"");
+        System.out.println(elements2);
+
+        //get Elements By tag
+        System.out.println("ELEMENTS BY TAG -> \"code\"");
+        Elements elements3 = doc.getElementsByTag("code");
+        System.out.println(elements3);
+
+
         for (Element element : elements) {
             System.out.println(element.text());
         }
