fa29bedec7ead0d5ab9c5f2d0b44c1739f26d1e3
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-04-24 10:09:11 +0900

    update!

--- src/main/java/org/example/ImageScraper.java
+++ src/main/java/org/example/ImageScraper.java
@@ -1,35 +1,29 @@
 package org.example;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import javax.swing.JFrame;
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class ImageScraper {
-
+    //array list String
+    static ArrayList<String> Images = new ArrayList<>();
     public static void main(String[] args) {
 
         String url = "https://en.wikipedia.org/wiki/Friedrich_Nietzsche";
+
         String saveDir = "./rsc"; // 保存先のディレクトリ
 
         try {
             // ウィキペディアのページにアクセス
             Document doc = Jsoup.connect(url).get();
-//            System.out.println("SUCCESS");
             System.out.println("TITILE IS -> " + doc.title().toUpperCase());
-
-            // 画像要素を取得
-            Element elements2 = doc.getElementById("hello");
 
             //Element get by class
             Elements elements = doc.getElementsByClass("thumbimage");
@@ -37,10 +31,14 @@ public class ImageScraper {
 
             for (Element element : elements) {
                 String imageUrl = element.attr("src");
-                System.out.println("IMAGE URL IS -> " + imageUrl);
-//                saveImage(imageUrl, saveDir);
+                System.out.println("IMAGE URL IS -> https:" + imageUrl);
+                Images.add("https:" + imageUrl);
+                String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+                System.out.println("FILE NAME IS -> " + fileName);
             }
 
+            saveImage(Images.get(0));
+            System.out.println("SUCCESS");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,32 +46,14 @@ public class ImageScraper {
     }
 
     // 画像を保存するメソッド
-    private static void saveImage(String imageUrl, String saveDir) throws IOException {
+    private static void saveImage(String imageUrl) throws IOException {
         // URL接続を開く
         URL url = new URL(imageUrl);
-        System.out.println(saveDir + imageUrl);
-        URLConnection conn = url.openConnection();
-
-        // ファイル名を取得
-        String fileName = url.getFile().substring(url.getFile().lastIndexOf("/") + 1);
-
-        // 保存先のディレクトリが存在しない場合は作成する
-        Path dirPath = Paths.get(saveDir);
-        if (!Files.exists(dirPath)) {
-            Files.createDirectories(dirPath);
-        }
-
-        // 画像を保存する
-        try (InputStream in = conn.getInputStream();
-             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(saveDir + fileName))) {
-
-            byte[] buf = new byte[1024];
-            int len;
-
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-        }
+        //URLからread
+        BufferedImage image = ImageIO.read(url);
+        File outPutFile = new File("./rsc/image.png");
+        //ファイルに保存
+        ImageIO.write(image, "png", outPutFile);
     }
 }
 
