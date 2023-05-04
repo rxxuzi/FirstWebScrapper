1feacaf7e5a0cfc3ced1dd41f2c3f1a67b2290fe
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-05-04 18:10:47 +0900

    update!

--- src/main/java/main/CoreX.java
+++ src/main/java/main/CoreX.java
@@ -13,7 +13,6 @@ import java.util.Random;
 public class CoreX extends JPanel {
     //検索ワードを設定
     public static String word;
-    public static Boolean start = false;
 
     final int corner = 6;
     static boolean isRunning = true;
@@ -30,7 +29,15 @@ public class CoreX extends JPanel {
     Scraper s = new Scraper(word);
     //テキストフィールド
     JTextField textField = new JTextField();
-    JLabel label = new JLabel(word);
+    //only number 1 ~ 100
+    JTextField numField = new JTextField();
+
+    JLabel label0 = new JLabel();
+    JLabel label1 = new JLabel("Get Images About :");
+    JLabel label2 = new JLabel("Max Images : ");
+    //テキストと数値が記入されているか
+    boolean b1 = false;
+    boolean b2 = false;
 
     CoreX(){
 
@@ -41,27 +48,45 @@ public class CoreX extends JPanel {
         btn1.setBounds(10,10,100,30);
         btn1.addActionListener(e -> {
             isRunning = true;
-            Scraper.run();
-            if(Scraper.isSuccess){
-                label.setForeground(Color.green);
-                label.setText("Success!!");
+            if (b1 &&  b2){
+                Scraper.run();
+                if(Scraper.isSuccess){
+                    label0.setForeground(Color.green);
+                    label0.setText("Success!!");
+                }
+                b1 = false;
+                b2 = false;
+                label1.setText("");
+                label2.setText("");
+            }else if(!b1 && b2){
+                label0.setForeground(Color.red);
+                label0.setText("Please input a word.");
+            } else if(b1){
+                label0.setForeground(Color.red);
+                label0.setText("Please input a number.");
+            }else {
+                label0.setForeground(Color.red);
+                label0.setText("Please input a word and a number.");
             }
         });
+        label1.setForeground(Color.CYAN);
+        label2.setForeground(Color.CYAN);
 
 
-        label.setBounds(10,90,200,30);
+        label0.setBounds(120,20,300,30);
 
+        label1.setBounds(10,70,200,30);
+        label2.setBounds(10,120,200,30);
 
         textField.setBounds(10,50,200,30);
 
-        //only number 1 ~ 100
-        JTextField numField = new JTextField();
-        numField.setBounds(10,120,90,30);
-        numField.setText("1");
+
+        numField.setBounds(10,100,90,30);
+        numField.setText("0");
+        //0~9以外の文字を除外する
         ((AbstractDocument) numField.getDocument()).setDocumentFilter(new DocumentFilter() {
             @Override
-            public void insertString(FilterBypass fb, int offset, String string,
-                                     AttributeSet attr) throws BadLocationException {
+            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
                 fb.insertString(offset, string.replaceAll("[^0-9]", ""), attr);
             }
 
@@ -72,39 +97,36 @@ public class CoreX extends JPanel {
             }
         });
         
-        JButton btn4 = new JButton("Set");
-        btn4.setBounds(100,120,70,30);
+        JButton btn4 = new JButton();
+        btn4.setBounds(100,100,30,30);
         btn4.addActionListener(e ->{
-            //get numfield value
             int num = Integer.parseInt(numField.getText());
             Scraper.maxImages = num;
             System.out.println(num);
+            label2.setText("Max Images : " + num);
+            b1 = true;
         });
 
+        //テキスト用ボタン
         JButton btn3 = new JButton();
         btn3.setBounds(210,50,30,30);
         btn3.addActionListener(e -> {
             word = textField.getText();
-            label.setForeground(Color.red);
-            label.setText(word);
+            label1.setForeground(Color.red);
+            label1.setText(word);
             Scraper.tag = textField.getText();
 
-            label.setForeground(Color.BLUE);
-            label.setText("Get Images About:" + word);
+            label1.setForeground(Color.CYAN);
+            label1.setText("Get Images About :" + word);
 
-            if (word != null){
-                start = true;
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-            }
+            b2 = true;
 
         });
 
-        this.add(label);
+        //描画
+        this.add(label0);
+        this.add(label1);
+        this.add(label2);
         this.add(btn1);
         this.add(btn3);
         this.add(btn4);
