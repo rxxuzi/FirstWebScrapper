a8434ae988c6c62db9c9a2dc8bbaabf1054a187c
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-05-04 16:28:05 +0900

    update!

---
+++ src/main/java/org/example/Eq.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Eq {
+    public static void main(String[] args) {
+        String s1 = "https://danbooru.me/posts?&page=2&tags=bikini";
+        String s2 = "https://danbooru.me/posts?page=4&tags=bikini";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,32 +14,71 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Objects;
 
 
 public class Main {
     static ArrayList<String> Images = new ArrayList<>();
     static String saveDirPath = "./rsc/pics/"; // 保存先のディレクトリのpath
     static int count = 0;
-    private static final String tag = "bikini";
+    public static final String tag = "bikini";
+    private static String tmpUrl = "";
+    private static final int pages = 2;
 
     public static void main(String[] args) throws IOException {
+
         String url = "https://danbooru.me/posts?tags=" + tag;
 
         Document docx = Jsoup.connect(url).get();
         System.out.println(docx.title());
-        //get img by  tag name = "img" and class = "has-cropped-true"
-        Elements Img = docx.getElementsByTag("img");
-        System.out.println("img count -> " + Img.size());
 
+        //get img by  tag name = "img" and class = "has-cropped-true"
+        Elements Img;
+
+        try{
+            for(int i = 1 ; i < pages+1  ; i++) {
+                //page数が2以上の時にurlを変える
+                if(i > 1){
+                    url = "https://danbooru.me/posts?page=" + i+ "&tags=" + tag;
+                }
+                //get url
+                docx = Jsoup.connect(url).get();
+                //get img by tag name = "img"
+                Img = docx.getElementsByTag("img");
+                System.out.println("img count -> " + Img.size());
+                System.out.println(url);
+                //今あるページ
+                getImageByElements(Img);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void getImageByElements(Elements Img) throws IOException {
         for (Element img : Img) {
-            System.out.println(img.attr("src"));
-            String imageUrl = "https://danbooru.me" + Img.attr("src");
+            //スクレイピング
+            String imageUrl = "https://danbooru.me" + img.attr("src");
+
+            //同じ画像を拾ってきたらbreak
+            if(Objects.equals(tmpUrl, imageUrl)){
+                System.out.println("Same file.");
+                break;
+            }
+
+            //tmpに保存
+            tmpUrl = imageUrl;
+
+            //画像URLを取得
             System.out.println("IMAGE URL IS -> " + imageUrl);
             Images.add(imageUrl);
+
+            //保存先を指定
             String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
             System.out.println("FILE NAME IS -> " + fileName);
             saveImage(imageUrl , fileName);
             count++;
+
         }
     }
     // 画像を保存するメソッド
@@ -48,10 +87,14 @@ public class Main {
         URL url = new URL(imageUrl);
         //URLからread
         BufferedImage image = ImageIO.read(url);
-        File outPutFile = new File(saveDirPath + tag + count+ ".png");
-        //ファイルに保存
-        ImageIO.write(image, "png", outPutFile);
+        if(image != null){
+            File outPutFile = new File(saveDirPath + tag + count+ ".png");
+            //ファイルに保存
+            ImageIO.write(image, "png", outPutFile);
 
-        System.out.println("("+count + ") SUCCESS -> " + outPutFile.getPath().toUpperCase());
+            System.out.println("("+count + ") SUCCESS -> " + outPutFile.getPath().toUpperCase());
+        }else{
+            System.out.println("("+count + ") FAIL -> " + imageUrl.toUpperCase());
+        }
     }
 }
